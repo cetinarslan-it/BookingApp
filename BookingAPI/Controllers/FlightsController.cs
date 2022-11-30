278bc89a22ebdab28605ce7f84b55a5ec3884a6f
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2022-11-30 10:42:06 +0100

    Add controller class

---
+++ BookingAPI/Controllers/FlightsController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace flightFinderApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class FlightsController : ControllerBase
+    {
+        private readonly FlightBookingDbContext _context;
+
+        public FlightsController(FlightBookingDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var flights = await _context.Flights
+                .Include(f => f.Itineraries)
+                .ThenInclude(i => i.Prices)
+                .ToListAsync();
+            return Ok(flights);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetById(string id)
+        {
+            if (id == null || _context.Flights == null)
+            {
+                return NotFound();
+            }
+
+            var flight = await _context.Flights
+                .Include(f => f.Itineraries)
+                .ThenInclude(i => i.Prices)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(flight);
+        }
+    }
+}
+
